867e7d3ce3ae38b975145c977c227efbe905e9f1
adnegro <andre.negro@oi.net.br>
2023-10-19 13:16:50 -0300

    end request

--- src/components/header/modalHeader.js
+++ src/components/header/modalHeader.js
@@ -1,33 +1,33 @@
 const arrCity = [
-  {
-    id: 4329,
-    city: "Salvador das Missões",
-    uf: "RS",
-    ddd: 55,
-    normalized: "salvador das missoes",
-  },
-  {
-    id: 4330,
-    city: "Salvador do Sul",
-    uf: "RS",
-    ddd: 51,
-    normalized: "salvador do sul",
-  },
-  {
-    id: 4882,
-    city: "São Salvador do Tocantins",
-    uf: "TO",
-    ddd: 63,
-    normalized: "sao salvador do tocantins",
-  },
-  {
-    id: 4328,
-    city: "Salvador",
-    uf: "BA",
-    ddd: 71,
-    normalized: "salvador",
-  },
-];
+	{
+		id: 4329,
+		city: 'Salvador das Missões',
+		uf: 'RS',
+		ddd: 55,
+		normalized: 'salvador das missoes'
+	},
+	{
+		id: 4330,
+		city: 'Salvador do Sul',
+		uf: 'RS',
+		ddd: 51,
+		normalized: 'salvador do sul'
+	},
+	{
+		id: 4882,
+		city: 'São Salvador do Tocantins',
+		uf: 'TO',
+		ddd: 63,
+		normalized: 'sao salvador do tocantins'
+	},
+	{
+		id: 4328,
+		city: 'Salvador',
+		uf: 'BA',
+		ddd: 71,
+		normalized: 'salvador'
+	}
+]
 
 const modalStructure = `<div class="header-modal_search_bar">
     <div class="max_width_container header-modal_input-content">
@@ -35,72 +35,91 @@ const modalStructure = `<div class="header-modal_search_bar">
       <button onclick="destroyModal()" >CANCELAR</button>
     </div>
   </div>
-  `;
+  `
 
 const selectCity = (city) => {
-  const fetchCity = arrCity.find(
-    (mapcity) => mapcity.id == city.dataset.cityid
-  );
-  const getHeaderText = document.getElementById("changeLocalization");
-  getHeaderText.dataset["normalized"] = city.dataset.normalized;
-  getHeaderText.innerHTML = `${fetchCity.city}, ${fetchCity.uf}`;
-  destroyModal();
-};
+	const fetchCity = arrCity.find((mapcity) => mapcity.id == city.dataset.cityid)
+	const getHeaderText = document.getElementById('changeLocalization')
+	getHeaderText.dataset['normalized'] = city.dataset.normalized
+	getHeaderText.innerHTML = `${fetchCity.city}, ${fetchCity.uf}`
+	destroyModal()
+}
 
 const showCitys = (arr) => {
-  const getUlContent = document.getElementById("container-city-modal");
-  getUlContent.innerHTML = "";
+	const getUlContent = document.getElementById('container-city-modal')
+	getUlContent.innerHTML = ''
 
-  arr.forEach((city, index) => {
-    const createRowLi = document.createElement("li");
-    const createRowButton = document.createElement("button");
-    createRowButton.setAttribute("class", "header-modal_li_buttons");
-    createRowButton.innerText = `${city.city.toUpperCase()}, ${city.uf.toUpperCase()}`;
-    createRowButton.dataset["cityid"] = city.id;
-    createRowButton.dataset["normalized"] = city.normalized;
+	arr.forEach((city, index) => {
+		const createRowLi = document.createElement('li')
+		const createRowButton = document.createElement('button')
+		createRowButton.setAttribute('class', 'header-modal_li_buttons')
+		createRowButton.innerText = `${city.city.toUpperCase()}, ${city.uf.toUpperCase()}`
+		createRowButton.dataset['cityid'] = city.id
+		createRowButton.dataset['normalized'] = city.normalized
 
-    createRowButton.autofocus = true;
+		createRowButton.autofocus = true
 
-    createRowButton.setAttribute("onclick", "selectCity(this)");
-    createRowLi.appendChild(createRowButton);
-    getUlContent.appendChild(createRowLi);
-  });
-  mappingFocusedButtons();
-};
+		createRowButton.setAttribute('onclick', 'selectCity(this)')
+		createRowLi.appendChild(createRowButton)
+		getUlContent.appendChild(createRowLi)
+	})
+	mappingFocusedButtons()
+}
 
-let getSearchInput = "";
+let getSearchInput = ''
 
 const searchCity = (e) => {
-  getSearchInput = e.value.toLowerCase();
+	getSearchInput = e.value.toLowerCase()
 
-  const newFilter = arrCity.filter(
-    (filterCity) =>
-      filterCity.normalized.toLowerCase().includes(getSearchInput) ||
-      filterCity.city.toLowerCase().includes(getSearchInput) ||
-      filterCity.uf.toLowerCase().includes(getSearchInput)
-  );
+	// making a fetch on backend
+	getSearchInput == ''
+		? showCitys(arrCity)
+		: fetchCitysOnBackend(getSearchInput)
+	// render cityes
+	// showCitys(newFilter)
 
-  // making a fetch on backend
-  fetchCitysOnBackend(e.value);
-  // render cityes
-  showCitys(newFilter);
+	// mapping focused buttons
+	mappingFocusedButtons()
+}
 
-  // mapping focused buttons
-  mappingFocusedButtons();
-};
+let breakingFetchRequest = false
 
-let breakingFetchRequest = false;
+
 const fetchCitysOnBackend = (city) => {
-  if (!breakingFetchRequest) {
-    breakingFetchRequest = true;
+	// instance api url handler
+	const apiUrl = new URL(
+		'https://homeoifibra-back-dev-hml.hml.ocpcorp.oi.intranet'
+	)
 
-    setTimeout(() => {
-      console.log("Requisição feita para:", getSearchInput);
-      console.log("Encoded url", encodeURI(getSearchInput));
-      breakingFetchRequest = false;
-    }, 1000);
-  }
-};
+	// // set pathname
+	console.log(city)
+	apiUrl.pathname = '/cities/name/' + city
+
+	if (!breakingFetchRequest) {
+		breakingFetchRequest = true
+
+		setTimeout(async () => {
+			try {
+				const requestAPi = () => {
+					fetch(apiUrl.href).then(async (request) => {
+						if (request.url != apiUrl.href) {
+							console.log('url', request.url)
+							console.log('href', apiUrl.href)
+							requestAPi()
+						}
+						request = await request.json()
+						request.length > 0 && showCitys(request)
+					})
+				}
+				requestAPi()
+			} catch (error) {
+				showCitys(arrCity)
+			}
+
+			breakingFetchRequest = false
+		}, 300)
+	}
+}
 
 // HOW TO GET OFFER
 
@@ -115,61 +134,60 @@ const fetchCitysOnBackend = (city) => {
 //   }
 // }
 
-const openModal = () => {
-  const createModal = document.createElement("div");
+function openModal() {
+	const createModal = document.createElement('div')
 
-  createModal.id = "header-modal-container";
-  createModal.setAttribute("class", "header-modal_city_modal");
-  createModal.innerHTML = modalStructure;
+	createModal.id = 'header-modal-container'
+	createModal.setAttribute('class', 'header-modal_city_modal')
+	createModal.innerHTML = modalStructure
 
-  document.body.appendChild(createModal);
+	document.body.appendChild(createModal)
 
-  const createUl = document.createElement("ul");
-  createUl.className = "max_width_container elUl";
-  createUl.id = "container-city-modal";
-  createModal.appendChild(createUl);
-  showCitys(arrCity);
-  document.getElementById("header-modal_inputcontent").focus();
+	const createUl = document.createElement('ul')
+	createUl.className = 'max_width_container elUl'
+	createUl.id = 'container-city-modal'
+	createModal.appendChild(createUl)
+	showCitys(arrCity)
+	document.getElementById('header-modal_inputcontent').focus()
 
-  mappingFocusedButtons();
-};
+	mappingFocusedButtons()
+}
 
 function destroyModal() {
-  const getModal = document.getElementById("header-modal-container");
-  document.body.removeChild(getModal);
+	const getModal = document.getElementById('header-modal-container')
+	document.body.removeChild(getModal)
 }
 
 function mappingFocusedButtons() {
-  const modal = document.getElementById("header-modal-container");
+	const modal = document.getElementById('header-modal-container')
 
-  function handleTabKey(e) {
-    const tagsModal = modal.querySelectorAll("input, ul, button");
+	function handleTabKey(e) {
+		const tagsModal = modal.querySelectorAll('input, ul, button')
 
-    const modalElements =
-      tagsModal.length <= 3
-        ? modal.querySelectorAll("input, button")
-        : modal.querySelectorAll("input, ul, button");
+		const modalElements =
+			tagsModal.length <= 3
+				? modal.querySelectorAll('input, button')
+				: modal.querySelectorAll('input, ul, button')
 
-    const firstElement = modalElements[0];
-    const lastElement = modalElements[modalElements.length - 1];
+		const firstElement = modalElements[0]
+		const lastElement = modalElements[modalElements.length - 1]
 
-    if (e.key === "Tab") {
-      if (e.shiftKey) {
-        if (document.activeElement === firstElement) {
-          e.preventDefault();
-          lastElement.focus();
-        }
-      } else {
-        if (document.activeElement === lastElement) {
-          e.preventDefault();
-          firstElement.focus();
-        }
-      }
-    }
-  }
-  modal.addEventListener("keydown", handleTabKey);
-
-  modal.onkeydown = function (e) {
-    e.key === "Escape" && destroyModal();
-  };
+		if (e.key === 'Tab') {
+			if (e.shiftKey) {
+				if (document.activeElement === firstElement) {
+					e.preventDefault()
+					lastElement.focus()
+				}
+			} else {
+				if (document.activeElement === lastElement) {
+					e.preventDefault()
+					firstElement.focus()
+				}
+			}
+		}
+	}
+	modal.addEventListener('keydown', handleTabKey)
+	modal.onkeydown = function (e) {
+		e.key === 'Escape' && destroyModal()
+	}
 }
