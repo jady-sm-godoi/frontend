49fd41ea35f47e3f363967f480934a544111a9f6
OI416745 <evani.silva@oi.net.br>
2023-10-30 19:05:53 -0300

    Added javascript oi saude

--- src/components/oisaude/oi_saude.js
+++ src/components/oisaude/oi_saude.js
@@ -1,139 +1,32 @@
+// const carousel = document.querySelector('.oi_saude__carousel');
+// const slides = document.querySelectorAll('.oi_saude__card');
+// const dots = document.querySelectorAll('.oi_saude__card_dot');
 
-const CARDS_WRAPPER = document.getElementById("servicos_oi__cardsWrapper");
-const CARD_SIZE = CARDS_WRAPPER.children[0].offsetWidth;
-const CARD_SIZE_WITH_MARGIN = CARD_SIZE + 24;
-const SCREEN_WIDTH = window.innerWidth;
+// let currentIndex = 0;
 
-// A classe Container entra apenas nessa resolução
-if (SCREEN_WIDTH >= 576) {
-    const CONTAINER_DISABLE = document.querySelector(
-        ".servicos_oi__container-disable"
-    );
-    CONTAINER_DISABLE.classList.add("container");
-}
+// dots.forEach((dot, index) => {
+//     dot.addEventListener('click', () => {
+//         goToSlide(index);
+//     });
+// });
 
-//Ajusta os valores para o max-width do css
-let fixWidthValue = 240;
-if (SCREEN_WIDTH >= 992) fixWidthValue = 300;
+// function goToSlide(index) {
+//     slides.forEach((slide, i) => {
+//         slide.style.transform = `translateX(-${index * 100}%)`;
+//         dots[i].classList.remove('oi_saude__card_dot_active');
+//     });
+//     dots[index].classList.add('oi_saude__card_dot_active');
+//     currentIndex = index;
+// }
 
-// Habiliata a escuta de evento do scroll
-let scrollListeningEnabled;
-CARDS_WRAPPER.addEventListener("touchmove", () => {
-    scrollListeningEnabled = true;
-});
+// function nextSlide() {
+//     currentIndex = (currentIndex + 1) % slides.length;
+//     goToSlide(currentIndex);
+// }
 
-//Atualiza as o dot atual de acordo com o scroll
-CARDS_WRAPPER.addEventListener("scroll", (event) => {
-    if (!scrollListeningEnabled) return;
+// function prevSlide() {
+//     currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+//     goToSlide(currentIndex);
+// }
 
-    const CARD_ITEM = document.querySelectorAll(".servicos_oi__card-box");
-    const ARRAY_CARD_ITEM = [...CARD_ITEM];
-
-    const ARRAY_CARD_WIDTH_VALUE = ARRAY_CARD_ITEM.map(
-        (card, index) => CARD_SIZE_WITH_MARGIN * index - fixWidthValue
-    );
-
-    const scrollLeft = event.target.scrollLeft;
-    let currentCard = 0;
-
-    ARRAY_CARD_WIDTH_VALUE.forEach((cardWidth, i) => {
-        if (scrollLeft >= cardWidth) currentCard = i;
-    });
-
-    let timeoutId;
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-        goToServiceCardIndexByDotColor({ value: currentCard });
-    }, 150);
-});
-
-//Rolagem para o card correspondente ao dot clicado
-const goToServiceCardIndexByDot = (e) => {
-    scrollListeningEnabled = false;
-    CARDS_WRAPPER.scrollLeft = CARD_SIZE_WITH_MARGIN * e.value;
-
-    let timeoutId;
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-        goToServiceCardIndexByDotColor(e);
-    }, 150);
-};
-
-//Atualiza a Cor do dot clicado para ativo
-const goToServiceCardIndexByDotColor = (e) => {
-    const carouselDots = document.getElementById(
-        "servicos_oi__carouselDotscontainer"
-    );
-    const dotsButtons = carouselDots.children;
-
-    [...dotsButtons].forEach((_, index) => {
-        dotsButtons[index].classList.remove("servicos_oi__card-radio-active");
-    });
-
-    const targetButton = dotsButtons[e.value];
-    if (targetButton)
-        targetButton.classList.add("servicos_oi__card-radio-active");
-};
-
-// Analytics
-function staticObserver(config) {
-    const {
-        DOMElement,
-        intersectingCallback = () => { },
-        unobserve = false,
-    } = config;
-
-    let observer;
-    function callback(entries) {
-        entries.forEach(({ isIntersecting }) => {
-            if (isIntersecting) intersectingCallback();
-            if (isIntersecting && unobserve) observer.unobserve();
-        });
-    }
-
-    observer = new IntersectionObserver(callback);
-    observer.observe(DOMElement);
-}
-
-const oiServiceCards = ["Oi Play TV", "Oi Saude", "Oi Expert"];
-
-const oiServiceItem = (item) => {
-    return {
-        item_id: item.toLowerCase().replaceAll(" ", "_"),
-        item_name: item,
-        item_brand: "Oi Fibra",
-        item_category: "Internet Addons",
-        item_list_name: "Internet Oi Servicos",
-        item_list_name: "Internet Oi Servicos",
-    };
-};
-
-function oiServiceEvent(event, items) {
-    return dataLayer.push({
-        event,
-        item_list_id: "internet_oi_servicos",
-        item_list_name: "Internet Oi Servicos",
-        items: items.map((service) => oiServiceItem(service)),
-    });
-}
-
-function viewOiService() {
-    return oiServiceEvent("view_item_list", oiServiceCards);
-}
-
-function selectOiService(itemName) {
-    return oiServiceEvent("select_item", [itemName]);
-}
-
-staticObserver({
-    DOMElement: document.querySelector(".servicos_oi__section"),
-    intersectingCallback: viewOiService,
-});
-
-[...document.querySelectorAll(".servicos_oi__cards_link")].forEach(
-    (cardBtn, index) => {
-        cardBtn.addEventListener("click", () => {
-            selectOiService(oiServiceCards[index]);
-        });
-    }
-);
+// setInterval(nextSlide, 5000); // Automatic slide change every 3 seconds
