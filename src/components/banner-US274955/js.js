b991280d4f9d49cb368957dc613799f4bb516c23
wnexous <andreneexus@hotmail.com>
2023-11-27 12:16:47 -0300

    fix carrouselhandler error

--- src/components/banner-US274955/js.js
+++ src/components/banner-US274955/js.js
@@ -1,10 +1,6 @@
 const carrouselHandler = new CarrouselHandlerV1('#banner-eofpav', {
 	enableAutoSkip: false,
-<<<<<<< HEAD
-	autoSkipDelay: 5000,
-=======
 	autoSkipDelay: 10000,
->>>>>>> 1ce3e09d10f2ca1ec31b5adf2a1f1d4bca363137
 	enableDots: true,
 	enableArrows: true
 })
